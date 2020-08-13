ed7768e93aa78278d92d65b495b2cc33124f9685
fabiosachetto <fabio@sachetto.com.br>
2020-08-13 19:47:21 -0300

    Subindo arquivos

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="VerdiFlix" title="VerdiFlix"></img>
+            </a>
+
+            <Button as="a" href="/" className="ButtonLink">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
