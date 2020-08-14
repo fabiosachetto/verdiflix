0af12a5ff80e2edac14ac477be7db4cf3a932760
fabiosachetto <fabio@sachetto.com.br>
2020-08-14 18:14:11 -0300

    Adicionado mais categorias

--- src/App.js
+++ src/App.js
@@ -18,7 +18,31 @@ function App() {
       <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
-      
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
       />
 
 
