6d7982bdfb3a525322a1f472b80c5b0b991a8488
fabiosachetto <fabio@sachetto.com.br>
2020-08-15 19:30:35 -0300

    adicionando  meu cv

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import CV from '../../assets/img/logo_linkedin.png';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.linkedin.com/in/fabioverdi/">
+      <a href="https://www.linkedin.com/in/fabioverdi/" target="_blank">
         <img className="CV" src={CV} alt="Meu LinkeDin" title="Meu LinkeDin"></img>
         
       </a>
