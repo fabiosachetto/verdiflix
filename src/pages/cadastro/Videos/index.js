3dbde43b0d6e9383a1112e7ee70ad7b9b32aa2d7
fabiosachetto <fabio@sachetto.com.br>
2020-08-17 20:34:06 -0300

    utilizando component page default

--- src/pages/cadastro/Videos/index.js
+++ src/pages/cadastro/Videos/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PageDefault from '../../../components/PageDefault';
 
 function CadastroVideo() {
     return (
-        <div>
-        Vc está na página de cadastro dos vídeos. <a href="/">Clique aqui para voltar para Home.</a>
-        </div>
+        <PageDefault>
+            Vc está na página de cadastro dos vídeos. <a href="/">Clique aqui para voltar para Home.</a>
+        </PageDefault>
     )
 }
 
