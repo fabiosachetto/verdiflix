c27807af655143b49a6723d389612e9b9a51c46e
fabiosachetto <fabio@sachetto.com.br>
2020-08-19 21:12:46 -0300

    pegando dados inseridos no input

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(["Teste"]);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState("Valor Inicial");
+
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria</h1>
+      <h1>Cadastrar Categoria: {nomeDaCategoria}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        console.log('Vc tentou enviar o formulário!', nomeDaCategoria);
+      }}>
+        {/*UseState*/}
         <label>
           Nome da Categoria:
-          <input type="text"/>
+          <input
+            type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandleQueOErroPediu(infosDoEvento) {
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
+          />
         </label>
 
-        <button>
-          Cadastrar
-        </button>
+        <button>Cadastrar</button>
       </form>
 
-      <Link to="/">Ir para a Home</Link>
+      <ul>
+        {categorias.map((categoria) => {
+          return (
+            <li key={categoria}>{nomeDaCategoria}</li>
+          );
+        })}
+      </ul>
 
+      <Link to="/">Ir para a Home</Link>
     </PageDefault>
   );
 }
