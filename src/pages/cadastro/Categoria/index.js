3a2c16d6a8ea00602b59790476b42305aab9eb83
fabiosachetto <fabio@sachetto.com.br>
2020-08-20 22:13:12 -0300

    Atualizando formulário e entendo o useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,38 +1,99 @@
 import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
+import FormField from "../../../components/FormField";
 
 function CadastroCategoria() {
-  const [categorias, setCategorias] = useState(["Teste"]);
-  const [nomeDaCategoria, setNomeDaCategoria] = useState("Valor Inicial");
+  const valoresIniciais = {
+    nome: "",
+    descricao: "",
+    cor: "",
+  }
+
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      //chave: pode ser nome, descrição... Enfim
+      [chave]: valor, //nome: 'valor'
+    });
+  };
+
+  function handleChange(infosDoEvento) {
+    const { getAttribute, value } = infosDoEvento.target;
+
+    setValue(
+      getAttribute('name'),
+      value
+    );
+  }
 
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria: {nomeDaCategoria}</h1>
+      <h1>Cadastrar Categoria: {values.nome}</h1>
 
-      <form onSubmit={function handleSubmit(infosDoEvento) {
-        infosDoEvento.preventDefault();
-        console.log('Vc tentou enviar o formulário!', nomeDaCategoria);
-      }}>
-        {/*UseState*/}
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            value={nomeDaCategoria}
-            onChange={function funcaoHandleQueOErroPediu(infosDoEvento) {
-              setNomeDaCategoria(infosDoEvento.target.value);
-            }}
-          />
-        </label>
+      <form
+        onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+
+          setCategorias([
+            /*OBS: Os ... é pra avisar pra pegar o q tem tudo ai q vc já escreveu, não joga fora não sobrescreve. */
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
+        }}>
+
+        <FormField />
+
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome}
+              name="nome"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
         <button>Cadastrar</button>
       </form>
 
       <ul>
-        {categorias.map((categoria) => {
+        {categorias.map((categoria, indice) => {
           return (
-            <li key={categoria}>{nomeDaCategoria}</li>
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
           );
         })}
       </ul>
