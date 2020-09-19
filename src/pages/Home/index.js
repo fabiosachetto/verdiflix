c6ffabe1e88d77d9b7b261b66bef60b2c3797569
fabiosachetto <fabio@sachetto.com.br>
2020-09-18 22:08:53 -0300

    Fazendo funcionar o bd fake

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+// import dadosIniciais from '../../data/dados_iniciais.json';'
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import PageDefault from '../../components/PageDefault';
+// import Footer from '../../components/Footer';
 import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
         console.log(categoriasComVideos);
+        setDadosIniciais(categoriasComVideos);
+      })
+
+      .catch((err) => {
+        console.log(err.message);
       });
-  });
+  }, []);
   // http://localhost:4040/categorias?_embed=videos
 
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      <BannerMain
+      {dadosIniciais.length >= 1 && (
+        <>
+          <BannerMain
+            videoTitle={dadosIniciais[0].videos[0].titulo}
+            url={dadosIniciais[0].videos[0].url}
+            videoDescription="Canal da Luna"
+          />
+
+          <Carousel
+            ignoreFirstVideo
+            category={dadosIniciais[0]}
+          />
+        </>
+      )}
+
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="Canal da Luna"
@@ -42,11 +65,9 @@ function Home() {
       <Carousel
         // ignoreFirstVideo
         category={dadosIniciais.categorias[3]}
-      />
+      /> */}
 
-      <Footer />
-
-    </div>
+    </PageDefault>
   );
 }
 
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,15 +1,19 @@
 import config from '../config';
 
-const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias?_embed=videos`;
+const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
 function getAllWithVideos() {
-  console.log(config.URL_BACKEND_TOP);
+  //console.log(config.URL_BACKEND_TOP);
 
-  return fetch(URL_CATEGORIES)
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaDoServidor) => {
-      const resposta = await respostaDoServidor.json();
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
 
-      return resposta;
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados!!!');
     });
 }
 
